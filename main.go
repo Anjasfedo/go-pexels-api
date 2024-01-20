f23010e56933161a2fcbc42fd0be39f65542a01a
AnjasFedo <fedoaf98@gmail.com>
2024-01-20 21:46:54 +0700

    Define Some Type Struct

--- main.go
+++ main.go
@@ -28,6 +28,36 @@ func NewClient(token string) *Client {
 	return &Client{Token: token, hc: c}
 }
 
+type SearchResult struct {
+	Page         int32   `json:"page"`
+	PerPage      int32   `json:"per_page"`
+	TotalResults int32   `json:"total_result"`
+	NextPage     string  `json:"next_page"`
+	Photos       []Photo `json:"photos"`
+}
+
+type Photo struct {
+	Id              int32       `json:"id"`
+	Width           int32       `json:"width"`
+	Height          int32       `json:"height"`
+	Url             string      `json:"url"`
+	Photographer    string      `json:"photographer"`
+	PhotographerUrl string      `json:"photographer_url"`
+	Src             PhotoSource `json:"src"`
+}
+
+type PhotoSource struct {
+	Original  string `json:"original"`
+	Large     string `json:"large"`
+	Large2x   string `json:"large2x"`
+	Medium    string `json:"medium"`
+	Small     string `json:"small"`
+	Potrait   string `json:"potrait"`
+	Square    string `json:"square"`
+	Landscape string `json:"landscape"`
+	Tiny      string `json:"tiny"`
+}
+
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
