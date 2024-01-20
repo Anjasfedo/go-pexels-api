a9b130627244ab81eeacc9dc3ae7b23449283938
AnjasFedo <fedoaf98@gmail.com>
2024-01-20 21:28:52 +0700

    Imported Packages & Create .env Load it with joho/godotenv

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-
+	"encoding/json"
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
-	
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env: %v", err)
+	}
 }
